34953298da5057b2e394c6ce26d0f6dc04b550bb
Thales Ramos <thlsrmsdev@gmail.com>
2020-11-12 22:58:15 +0100

    Added express json parse to the app

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-
-const port = process.env.PORT || 49990;
+const port = process.env.PORT || 3000;
 // express app
 const app = express();
 
 // listen for requests
-app.listen(port, () => {
+app.listen(port, (err) => {
+    if (err) return console.log(err);
     console.log(`Listening to port ${port}`);
 });
 
@@ -15,6 +15,8 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json()); // Parse JSON bodies
 
 app.get('/', (req, res) => {
     res.render('index', {title: 'Home'});
